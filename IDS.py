f60c5e6ea156031d6971f70e772238756e5d7f64
KostasEreksonas <k.ereksonas@gmail.com>
2023-11-28 10:10:05 +0200

    Moved code to functions

--- IDS.py
+++ IDS.py
@@ -8,30 +8,36 @@ import seaborn as sns
 import time
 
 # Read feature names
-with open("dataset/kddcup.names", 'r') as f:
-    cols = f.read()
-cols = cols.split()
-columns = []
-for col in cols:
-    columns.append(col)
-
-print(f"Features: {columns}\nLength: {len(columns)}")
+def features():
+    with open("dataset/kddcup.names", 'r') as f:
+        cols = f.read()
+    cols = cols.split()
+    columns = []
+    for col in cols:
+        columns.append(col)
+    return columns
 
 # Read attack types
-with open("dataset/attack.types", 'r') as f:
-    attacks = f.read()
+def attacks():
+    with open("dataset/attack.types", 'r') as f:
+        attacks = f.read()
+    attacks = attacks.split()
+    attack_name,attack_type = [[] for x in range(2)]
+    attack_dict = {}
+    for x in range(0,len(attacks)):
+        if x == 0:
+            attack_name.append(attacks[x])
+        elif x % 2 == 0:
+            attack_name.append(attacks[x])
+        elif x % 2 != 0:
+            attack_type.append(attacks[x])
+    for x in range(0,len(attack_type)):
+        attack_dict[attack_name[x]] = attack_type[x]
+    return attack_dict
 
-attacks = attacks.split()
-attack_name,attack_type = [[] for x in range(2)]
-attack_dict = {}
-for x in range(0,len(attacks)):
-    if x == 0:
-        attack_name.append(attacks[x])
-    elif x % 2 == 0:
-        attack_name.append(attacks[x])
-    elif x % 2 != 0:
-        attack_type.append(attacks[x])
-print(f"attack_name: {attack_name}, attack_type {attack_type}")
-for x in range(0,len(attack_type)):
-    attack_dict[attack_name[x]] = attack_type[x]
-print(attack_dict)
+def main():
+    print(f"Features: {features()}")
+    print(f"Attacks: {attacks()}")
+
+if __name__ == "__main__":
+    main()
