b0d193d62262260457e3455afef1ba92a1b89f31
KostasEreksonas <k.ereksonas@gmail.com>
2023-11-28 22:38:00 +0200

    One-hot encoded textual data

--- IDS.py
+++ IDS.py
@@ -6,6 +6,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import time
+from sklearn.model_selection import train_test_split 
+from sklearn.preprocessing import MinMaxScaler 
 
 # Read feature names
 def features():
@@ -50,11 +52,25 @@ def get_correlation(df):
     sns.heatmap(corr)
     plt.show()
 
+def map_features(path, columns, rows):
+    df = add_feature(path, columns, rows)
+    pmap = {'icmp':0, 'tcp':1, 'udp':2}
+    fmap = {'SF':0, 'S0':1, 'REJ':2, 'RSTR':3, 'RSTO':4, 'SH':5, 'S1':6, 'S2':7, 'RSTOS0':8, 'S3':9, 'OTH':10}
+    smap = {'aol':0, 'auth':1, 'bgp':2, 'courier':3, 'csnet_ns':4, 'ctf':5, 'daytime':6, 'discard':7, 'domain':8, 'domain_u':9, 'echo':10, 'eco_i':11, 'ecr_i':12, 'efs':13, 'exec':14, 'finger':15, 'ftp':16, 'ftp_data':17, 'gopher':18, 'harvest':19, 'hostnames':20, 'http':21, 'http_2784':22, 'http_443':23, 'http_8001':24, 'imap4':25, 'IRC':26, 'iso_tsap':27, 'klogin':28, 'kshell':29, 'ldap':30, 'link':31, 'login':32, 'mtp':33, 'name':34, 'netbios_dgm':35, 'netbios_ns':36, 'netbios_ssn':37, 'netstat':38, 'nnsp':39, 'nntp':40, 'ntp_u':41, 'other':42, 'pm_dump':43, 'pop_2':44, 'pop_3':45, 'printer':46, 'private':47, 'red_i':48, 'remote_job':49, 'rje':50, 'shell':51, 'smtp':52, 'sql_net':53, 'ssh':54, 'sunrpc':55, 'supdup':56, 'systat':57, 'telnet':58, 'tftp_u':59, 'time':60, 'tim_i':61, 'urh_i':62, 'urp_i':63, 'uucp':64, 'uucp_path':65, 'vmnet':66, 'whois':67, 'X11':68, 'Z39_50':69}
+    cmap = {'back':0, 'land':1, 'neptune':2, 'pod':3, 'smurf':4, 'teardrop':5, 'processtable':6, 'udpstorm':7, 'mailbomb':8, 'apache2':9, 'ipsweep':10, 'mscan':11, 'nmap':12, 'portsweep':13, 'saint':14, 'satan':15, 'ftp_write':16, 'guess_passwd':17, 'imap':18, 'multihop':19, 'phf':20, 'warezmaster':21, 'warezclient':22, 'spy':23, 'sendmail':24, 'xlock':25, 'snmpguess':26, 'named':27, 'xsnoop':28, 'snmpgetattack':29, 'worm':30, 'buffer_overflow':31, 'loadmodule':32, 'perl':33, 'rootkit':34, 'xterm':35, 'ps':36, 'httptunnel':37, 'sqlattack':38, 'normal':39}
+    tmap = {'normal':0, 'dos':1, 'probe':2, 'r2l':3, 'u2r':3}
+    df['protocol_type'] = df['protocol_type'].map(pmap)
+    df['flag'] = df['flag'].map(fmap)
+    df['service'] = df['service'].map(smap)
+    df['class'] = df['class'].map(cmap)
+    df['Attack Type'] = df['Attack Type'].map(tmap)
+    return df
+
 def main():
     path = "dataset/KDDTrain+_20Percent.txt"
     columns = features()
     rows = attacks()
-    df = add_feature(path, columns, rows)
+    df = map_features(path, columns, rows)
     print(df)
 
 if __name__ == "__main__":
