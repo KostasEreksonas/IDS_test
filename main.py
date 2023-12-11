13f1f4378dcf7a40b6598ce741510d05c60efe8c
KostasEreksonas <k.ereksonas@gmail.com>
2023-12-11 21:54:15 +0200

    Main function

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ import NSL_KDD
 import models
 
 def main():
-    path = "data/NSL_KDD/KDDTrain+.txt"
-    df = NSL_KDD.dataframe(path)
-    shape = NSL_KDD.shape(path)
-    is_null = NSL_KDD.isnull(path)
-    print(f"Dataframe:\n{df}\nShape: {shape}\nIs null:\n{is_null}")
     models.neural_network()
 
 if __name__ == "__main__":
