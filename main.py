cd43f11fea9cd28078bac2068b997c6015596d08
KostasEreksonas <k.ereksonas@gmail.com>
2023-12-12 19:53:01 +0200

    Draw plots

--- main.py
+++ main.py
@@ -3,12 +3,20 @@
 import models
 import plots
 
+def draw_plots():
+    path = ["data/NSL_KDD/KDDTrain+.txt",
+            "data/NSL_KDD/KDDTest+.txt",
+            "data/NSL_KDD/KDDTrain+_20Percent.txt",
+            "data/NSL_KDD/KDDTest-21.txt"]
+    for x in path:
+        dataset_name = x.split("/")[-1].split(".")[0]
+        plots.bar_graph(x, dataset_name, "protocol_type", "Type", "Occurrences", "Protocol occurences by type")
+        plots.bar_graph(x, dataset_name, "attack_type", "Type", "Occurrences", "Attack occurences by type")
+        plots.bar_graph(x, dataset_name, "logged_in", "Logged In (1 - Yes, 0 - No)", "Occurrences", "Success")
+        plots.correlation(dataset_name, x)
+
 def main():
-    path = "data/NSL_KDD/KDDTrain+.txt"
-    plots.bar_graph(path, "protocol_type", "Type", "Occurrences", "Protocol occurences by type")
-    plots.bar_graph(path, "attack_type", "Type", "Occurrences", "Attack occurences by type")
-    plots.bar_graph(path, "logged_in", "Logged In (1 - Yes, 0 - No)", "Occurrences", "Success")
-    #plots.correlation(path)
+    draw_plots()
     #models.neural_network()
 
 if __name__ == "__main__":
--- plots.py
+++ plots.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from tensorflow.keras.utils import to_categorical, plot_model
 
-def bar_graph(path, column_name, xlabel, ylabel, title):
+def bar_graph(path, dataset_name, column_name, xlabel, ylabel, title):
     """Draw a bar graph"""
     df = NSL_KDD.dataframe(path)
     data = {}
@@ -19,23 +19,23 @@ def bar_graph(path, column_name, xlabel, ylabel, title):
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.grid(visible=None, which='both', axis='both')
-    plt.savefig(f"plots/features/{column_name}.png")
-    print(f"Plot saved to plots/features/{column_name}.png")
+    plt.savefig(f"plots/features/{dataset_name}-{column_name}.png")
+    print(f"Plot saved to plots/features/{dataset_name}-{column_name}.png")
 
 def block_scheme(model, path):
     """Draw a block scheme"""
     plot_model(model, to_file=path)
     print(f"[+] Block scheme saved at: {path}")
 
-def correlation(path):
+def correlation(dataset_name, path):
     """Draw a heatmap"""
     df = NSL_KDD.dataframe(path)
     df = df[[col for col in df if df[col].nunique() > 1]] # keep columns where there are more than 1 unique values
     corr = df.corr(numeric_only=True)
     plt.figure(figsize =(15, 12))
     sns.heatmap(corr)
-    plt.savefig(f"plots/features/heatmap.png")
-    print(f"Plot saved to plots/features/heatmap.png")
+    plt.savefig(f"plots/features/{dataset_name}-heatmap.png")
+    print(f"Plot saved to plots/features/{dataset_name}-heatmap.png")
 
 def line_graph(data, line1, line2, title, ylabel, xlabel, filename):
     """Plot a line graph for training and testing results"""
